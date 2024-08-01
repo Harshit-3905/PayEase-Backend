e28081df642f9a8739e0f334c6d4be7ad5ecf39f
Harshit Joshi <harshitjoshileet@gmail.com>
2024-08-01 18:42:16 +0530

    Add user and account routes to server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/db";
+import cors from "cors";
+
+dotenv.config();
+connectDB();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+import userRoutes from "./routes/user.routes";
+import accountRoutes from "./routes/account.routes";
+
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/account", accountRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
